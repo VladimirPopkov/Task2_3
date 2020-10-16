420778ffde8570dd089ef9c36c20199f23d769d1
VladimirPopkov <vovanpopkov@mail.ru>
2020-10-16 16:01:58 +0300

    Созданы массивы и выведены на экран

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,25 @@
 package com.company;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int positive[] = new int[7];
+        int negative[] = new int[7];
+        Random random = new Random();
+        int k = 0;
+        while (k<7){
+            positive[k] = random.nextInt(100) + 1;
+            negative[k] = random.nextInt(100) - 100;
+            k++;
+        }
+        for (int i = 0;i < positive.length;i++){
+            System.out.printf("%d ", positive[i]);
+        }
+        System.out.println();
+        for (int i = 0;i < negative.length;i++){
+            System.out.printf("%d ", negative[i]);
+        }
     }
 }
