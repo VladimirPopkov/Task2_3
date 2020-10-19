0e0d38ed01fec8c17c704ba373687a5a977f9d0e
VladimirPopkov <vovanpopkov@mail.ru>
2020-10-19 09:37:32 +0300

    Задание выполнено

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
         int positive[] = new int[7];
         int negative[] = new int[7];
+        int summArray[] = new int[7];
         Random random = new Random();
         int k = 0;
         while (k<7){
@@ -14,12 +15,20 @@ public class Main {
             negative[k] = random.nextInt(100) - 100;
             k++;
         }
+        System.out.printf("Массив положительных чисел: ");
         for (int i = 0;i < positive.length;i++){
             System.out.printf("%d ", positive[i]);
         }
         System.out.println();
+        System.out.printf("Массив отрицательных чисел: ");
         for (int i = 0;i < negative.length;i++){
             System.out.printf("%d ", negative[i]);
+        }
+        System.out.println();
+        System.out.printf("Суммарный массив: ");
+        for (int i = 0;i < summArray.length;i++){
+            summArray[i] = positive[i] + negative[i];
+            System.out.printf("%d ", summArray[i]);
         }
     }
 }
