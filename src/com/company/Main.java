9ecc3eedc1cbc46e0e5a702897111a60be0f3af6
VladimirPopkov <vovanpopkov@mail.ru>
2020-11-21 14:56:55 +0300

    Код разделен на методы

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,31 +4,40 @@ import java.util.Random;
 
 public class Main {
 
+    private final static int ARRAY_SIZE = 7;
+
     public static void main(String[] args) {
-        int positive[] = new int[7];
-        int negative[] = new int[7];
-        int summArray[] = new int[7];
+        int positive[] = new int[ARRAY_SIZE];
+        int negative[] = new int[ARRAY_SIZE];
+        int sumArray[] = new int[ARRAY_SIZE];
+        fillArray(positive, 1, 100);
+        fillArray(negative, -100, -1);
+        printArr("Массив положительных чисел: ", positive);
+        printArr("Массив отрицательных чисел: ", negative);
+        sumArray(sumArray,positive,negative);
+        printArr("Суммарный массив: ", sumArray);
+    }
+
+    static void printArr(String nameArr, int[] arr) {
+        System.out.print(nameArr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("%d ", arr[i]);
+        }
+        System.out.println();
+    }
+
+    static void fillArray(int[] arr, int min, int max){
         Random random = new Random();
         int k = 0;
         while (k<7){
-            positive[k] = random.nextInt(100) + 1;
-            negative[k] = random.nextInt(100) - 100;
+            arr[k] = random.nextInt(max - min + 1) + min;
             k++;
         }
-        System.out.printf("Массив положительных чисел: ");
-        for (int i = 0;i < positive.length;i++){
-            System.out.printf("%d ", positive[i]);
-        }
-        System.out.println();
-        System.out.printf("Массив отрицательных чисел: ");
-        for (int i = 0;i < negative.length;i++){
-            System.out.printf("%d ", negative[i]);
-        }
-        System.out.println();
-        System.out.printf("Суммарный массив: ");
-        for (int i = 0;i < summArray.length;i++){
-            summArray[i] = positive[i] + negative[i];
-            System.out.printf("%d ", summArray[i]);
+    }
+
+    static void sumArray(int[] sumArr, int[] arr1, int[] arr2){
+        for (int i = 0;i < sumArr.length;i++){
+            sumArr[i] = arr1[i] + arr2[i];
         }
     }
 }
